a2e26f596fdd6de9342bdc56d8069349c96dcbc8
mdwasifulkabir <wasifulkabir9016@gmail.com>
2026-02-01 16:02:44 +0800

    Import firebase sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ4SNMLM0RajxvoXdkH3NgT2frn3CHIb0",
+  authDomain: "negotiation-trainer-4535e.firebaseapp.com",
+  projectId: "negotiation-trainer-4535e",
+  storageBucket: "negotiation-trainer-4535e.firebasestorage.app",
+  messagingSenderId: "72572267625",
+  appId: "1:72572267625:web:b6f3557a3516ebf67cc6f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function TopBar() {
   return (
     <header>
