552190d320f789aa72c0ae6c158c1b53bd2d5a08
mdwasifulkabir <wasifulkabir9016@gmail.com>
2026-02-02 13:05:47 +0800

    Create sign in and sign out functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {} from "firebase/firestore";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  connectAuthEmulator,
+} from "firebase/auth";
 
-// Your web app's Firebase configuration
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCZ4SNMLM0RajxvoXdkH3NgT2frn3CHIb0",
   authDomain: "negotiation-trainer-4535e.firebaseapp.com",
@@ -15,7 +19,6 @@ const firebaseConfig = {
   appId: "1:72572267625:web:b6f3557a3516ebf67cc6f8",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
@@ -46,11 +49,20 @@ function ChatPage() {
   );
 }
 
+function SignIn() {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  return <button onClick={googleSignIn}>Sign In With Google</button>;
+}
+
 export default function App() {
+  const [user] = useAuthState(auth);
   return (
     <>
       <TopBar />
-      <ChatPage />
+      <section>{user ? <ChatPage /> : <SignIn />}</section>
     </>
   );
 }
